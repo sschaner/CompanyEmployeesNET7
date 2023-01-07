79a0c6d5fa0b2d14c12657ecb48ff29e761f434d
Steve Schaner <schanerst@gmail.com>
2023-01-07 12:52:54 -0500

    Add project files.

---
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace CompanyEmployees.Extensions
+{
+    public static class ServiceExtensions
+    {
+        /// <summary>Configures the cors.</summary>
+        /// <param name="services">The services.</param>
+        public static void ConfigureCors(this IServiceCollection services) =>
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+            });
+
+        /// <summary>Configures the IIS integration.</summary>
+        /// <param name="services">The services.</param>
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+            services.Configure<IISOptions>(options =>
+            {
+
+            });
+    }
+}
